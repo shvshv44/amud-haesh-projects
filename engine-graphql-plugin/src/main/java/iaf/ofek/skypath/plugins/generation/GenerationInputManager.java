f3f61538b41f3c5f0622d2032c700c4e0099761f
Shaked <shvshv44@gmail.com>
2020-03-18 23:50:56 +0200

    feat: * Add process implementation * fix UI

--- engine-graphql-plugin/src/main/java/iaf/ofek/skypath/plugins/generation/GenerationInputManager.java
+++ engine-graphql-plugin/src/main/java/iaf/ofek/skypath/plugins/generation/GenerationInputManager.java
@@ -12,17 +12,22 @@ public class GenerationInputManager {
     private GraphqlSchemaParser graphqlSchemaParser;
 
     public GraphqlSchema startGenerationInput() {
+
+        showInputWindow();
+
+        return graphqlSchemaParser.parse("TODO");
+    }
+
+    private Window showInputWindow() {
         final DialogBuilder dialogBuilder = new DialogBuilder();
         final Window window = dialogBuilder.getWindow();
-        window.setSize(500,500);
         GraphqlSchemaInputWindow inputWindow = new GraphqlSchemaInputWindow();
         dialogBuilder.setCenterPanel(inputWindow.getInputPanel());
         dialogBuilder.setTitle("Graphql Schema Input");
         dialogBuilder.removeAllActions();
         dialogBuilder.show();
 
-
-        return graphqlSchemaParser.parse("TODO");
+        return window;
     }
 
 
