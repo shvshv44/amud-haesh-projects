1f2fa26616ec13f1344ccb227c34e30e204e94c6
Shaked <shvshv44@gmail.com>
2020-03-21 15:30:58 +0200

    feat: * finish grpahql field without scalars

--- engine-graphql-plugin/src/main/java/org/shaq/plugins/generation/GraphQLGenerationContextAdapter.java
+++ engine-graphql-plugin/src/main/java/org/shaq/plugins/generation/GraphQLGenerationContextAdapter.java
@@ -2,6 +2,9 @@ package org.shaq.plugins.generation;
 
 import graphql.language.*;
 import graphql.schema.idl.TypeDefinitionRegistry;
+import org.shaq.plugins.exceptions.GraphQLSchemaParseException;
+import org.shaq.plugins.models.graphql.GraphQLField;
+import org.shaq.plugins.models.graphql.GraphQLFieldType;
 import org.shaq.plugins.models.graphql.GraphQLGenerationContext;
 import org.shaq.plugins.models.graphql.GraphQLSimpleType;
 
@@ -16,7 +19,7 @@ public class GraphQLGenerationContextAdapter {
 
         buildSimpleTypes(context, registry.types());
         buildTypeExtensions(context, registry.types());
-        buildFields(context, registry.types());
+        buildFields(context, registry.types()); // TODO: implement deal with scalars
 
         return context;
     }
@@ -27,25 +30,68 @@ public class GraphQLGenerationContextAdapter {
 
             GraphQLSimpleType simpleType = context.getTypes().get(typeDefinition.getName());
             if (typeDefinition instanceof ObjectTypeDefinition) {
-                dealWithTypeFields(simpleType, ((ObjectTypeDefinition) typeDefinition).getFieldDefinitions());
+                dealWithTypeFields(context, simpleType, ((ObjectTypeDefinition) typeDefinition).getFieldDefinitions());
             } else if (typeDefinition instanceof InterfaceTypeDefinition) {
-                dealWithTypeFields(simpleType, ((InterfaceTypeDefinition) typeDefinition).getFieldDefinitions());
+                dealWithTypeFields(context, simpleType, ((InterfaceTypeDefinition) typeDefinition).getFieldDefinitions());
             }
 
         }
 
     }
 
-    private void dealWithTypeFields(GraphQLSimpleType simpleType, List<FieldDefinition> typeFieldDefinitions) {
+    private void dealWithTypeFields(GraphQLGenerationContext context, GraphQLSimpleType simpleType, List<FieldDefinition> typeFieldDefinitions) {
         for (FieldDefinition fieldDefinition : typeFieldDefinitions) {
-            addFieldFromDefinition(simpleType, fieldDefinition);
+            addFieldFromDefinition(context, simpleType, fieldDefinition);
         }
     }
 
-    private void addFieldFromDefinition(GraphQLSimpleType simpleType, FieldDefinition fieldDefinition) {
+    private void addFieldFromDefinition(GraphQLGenerationContext context, GraphQLSimpleType simpleType, FieldDefinition fieldDefinition) {
+        GraphQLField field = new GraphQLField();
+        field.setName(fieldDefinition.getName());
+        GraphQLFieldType fieldType = createFieldType(context, fieldDefinition.getType());
+        field.setType(fieldType);
+        simpleType.getFields().put(field.getName(), field);
+    }
 
-        //TODO: implement
+    private GraphQLFieldType createFieldType(GraphQLGenerationContext context, Type type) {
 
+        if(type instanceof TypeName) {
+            return createFieldTypeFromTypeName(context,((TypeName)type).getName(), false, true);
+        }
+        if(type instanceof NonNullType) {
+            return createFieldTypeFromNonTypeName(context, ((NonNullType)type).getType() , false, false);
+        }
+        if(type instanceof ListType) {
+            return createFieldTypeFromNonTypeName(context, ((ListType)type).getType() , true, true);
+        }
+
+        throw new GraphQLSchemaParseException(type.getClass() + " is not implemented in this current version parser!");
+    }
+
+    private GraphQLFieldType createFieldTypeFromNonTypeName(GraphQLGenerationContext context, Type innerType, boolean isCollection, boolean isNullable) {
+        if(innerType instanceof TypeName) {
+            return createFieldTypeFromTypeName(context,((TypeName)innerType).getName(), isCollection, isNullable);
+        }
+        if(innerType instanceof NonNullType) {
+            return createFieldTypeFromNonTypeName(context, ((NonNullType)innerType).getType() , isCollection, false);
+        }
+        if(innerType instanceof ListType) {
+            return createFieldTypeFromNonTypeName(context, ((ListType)innerType).getType() , true, isNullable);
+        }
+
+        throw new GraphQLSchemaParseException(innerType.getClass() + " is not implemented in this current version parser!");
+    }
+
+    private GraphQLFieldType createFieldTypeFromTypeName(GraphQLGenerationContext context, String core, boolean isCollection, boolean isNullable) {
+        GraphQLSimpleType coreType = context.getTypes().get(core);
+        if (coreType == null)
+            throw new GraphQLSchemaParseException(core + " type does not exist in schema!");
+
+        GraphQLFieldType graphQLFieldType = new GraphQLFieldType();
+        graphQLFieldType.setCoreType(coreType);
+        graphQLFieldType.setIsCollection(isCollection);
+        graphQLFieldType.setIsNullable(isNullable);
+        return graphQLFieldType;
     }
 
 
