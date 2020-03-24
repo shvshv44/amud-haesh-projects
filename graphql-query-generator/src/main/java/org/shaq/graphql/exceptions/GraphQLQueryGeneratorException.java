52937256ea17cacb79abcc824fda130cbf57034b
amittes <amitt886@gmail.com>
2020-03-24 12:13:16 +0200

    feat: graphql serialized name

--- graphql-query-generator/src/main/java/org/shaq/graphql/exceptions/GraphQLQueryGeneratorException.java
+++ graphql-query-generator/src/main/java/org/shaq/graphql/exceptions/GraphQLQueryGeneratorException.java
@@ -1,12 +1,5 @@
 package org.shaq.graphql.exceptions;
 
-/**
- * Created with IntelliJ IDEA.
- * User: shakedvit
- * Date: 25/07/18
- * Time: 08:13
- * To change this template use File | Settings | File Templates.
- */
 public class GraphQLQueryGeneratorException extends RuntimeException {
 
     public GraphQLQueryGeneratorException() {
