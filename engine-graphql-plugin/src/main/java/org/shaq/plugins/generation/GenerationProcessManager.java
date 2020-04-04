71e376c0102d3dae31cfb778fc7ad5a95c5e0466
Shaked <shvshv44@gmail.com>
2020-04-04 15:25:04 +0300

    feat: * enums

--- engine-graphql-plugin/src/main/java/org/shaq/plugins/generation/GenerationProcessManager.java
+++ engine-graphql-plugin/src/main/java/org/shaq/plugins/generation/GenerationProcessManager.java
@@ -1,11 +1,18 @@
 package org.shaq.plugins.generation;
 
+import cucumber.api.java.gl.E;
+import gherkin.lexer.En;
+import org.shaq.plugins.models.graphql.GraphQLFieldType;
 import org.shaq.plugins.models.graphql.GraphQLOperation;
+import org.shaq.plugins.models.graphql.GraphQLSimpleType;
 import org.shaq.plugins.models.javafile.FileJavaComponent;
 import org.shaq.plugins.models.javafile.clazz.ClassFile;
 import org.shaq.plugins.models.javafile.clazz.FieldData;
 import org.shaq.plugins.models.javafile.common.ModifierType;
+import org.shaq.plugins.models.javafile.enums.EnumFile;
+import org.shaq.plugins.models.javafile.enums.EnumValue;
 import org.shaq.plugins.models.logic.ProjectModel;
+import org.shaq.plugins.models.user.GraphQLChoosenField;
 import org.shaq.plugins.models.user.UserChoiceGraphQLContext;
 
 import java.util.ArrayList;
@@ -15,23 +22,54 @@ import java.util.List;
 public class GenerationProcessManager {
 
     private HashSet<String> usedClassNames;
+    private ArrayList<FileJavaComponent> javaComponents;
 
     public List<FileJavaComponent> startGeneration(UserChoiceGraphQLContext userChoices, ProjectModel projectModel) {
-        ArrayList <FileJavaComponent> javaComponents = new ArrayList<>();
+        javaComponents = new ArrayList<>();
         usedClassNames = new HashSet<>();
 
         FileJavaComponent operationMainClass = createOperationClass(userChoices, projectModel);
         javaComponents.add(operationMainClass);
 
-        createAllTypesClass(userChoices, javaComponents);
+        createAllTypesClass(userChoices.getRootField(), userChoices.getRootField().getOriginalField().getType().getCoreType().getName() , projectModel);
 
         return javaComponents;
     }
 
-    private void createAllTypesClass(UserChoiceGraphQLContext userChoices, ArrayList<FileJavaComponent> javaComponents) {
+    private void createAllTypesClass(GraphQLChoosenField rootField, String classChoosenName, ProjectModel projectModel) {
 
-        //TODO: implement
+        GraphQLSimpleType fieldType = rootField.getOriginalField().getType().getCoreType();
 
+        if(isFieldEnum(rootField))
+            buildEnumField(rootField, classChoosenName, projectModel);
+        else if (isFieldRegularClass(rootField)) {
+            buildRegularClassFields(rootField, classChoosenName, projectModel);
+        }
+    }
+
+    private void buildRegularClassFields(GraphQLChoosenField rootField, String classChoosenName, ProjectModel projectModel) {
+        //TODO : implemwent
+    }
+
+    private void buildEnumField(GraphQLChoosenField rootField, String classChoosenName, ProjectModel projectModel) {
+        EnumFile enumFile = new EnumFile();
+        enumFile.setName(classChoosenName);
+        enumFile.setPackagePath(projectModel.getPackageName());
+        enumFile.setValues(new ArrayList<>());
+
+        for (GraphQLChoosenField enumValue : rootField.getInnerChoosenFields()) {
+            //TODO : set enums
+        }
+    }
+
+    private boolean isFieldEnum (GraphQLChoosenField field) {
+        return field.getOriginalField().getType().getCoreType().getIsEnum() &&
+                !field.getOriginalField().getType().getCoreType().getIsScalar();
+    }
+
+    private boolean isFieldRegularClass (GraphQLChoosenField field) {
+        return !field.getOriginalField().getType().getCoreType().getIsEnum() &&
+                !field.getOriginalField().getType().getCoreType().getIsScalar();
     }
 
     private FileJavaComponent createOperationClass(UserChoiceGraphQLContext userChoices, ProjectModel projectModel) {
